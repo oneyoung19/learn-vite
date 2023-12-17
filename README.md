b71803d19623623377f04cb7b9ecfa01ff625a6e
yangxiaoping <2878847069@qq.com>
2023-12-17 23:58:22 +0800

    chore: 处理less

--- README.md
+++ README.md
@@ -8,4 +8,13 @@ Learn Vite
 
 - [ ] 如果 `vite` 的 `index.html` 放置在根目录下，而不是 `public` 目录下的话，那么其他托管文件如何放置呢？
 - [ ] 如何指定入口文件？譬如 `src/main.js`
-- [ ] `vite` 能使用 `vue2` 么？与 `TS` 又有什么联系和限制？
+- [ ] `vite` 能使用 `vue2` 么？
+Vite 为 Vue 提供第一优先级支持：
+
+Vue 3 单文件组件支持：@vitejs/plugin-vue
+Vue 3 JSX 支持：@vitejs/plugin-vue-jsx
+Vue 2.7 SFC 支持：@vitejs/plugin-vue2
+Vue 2.7 JSX support via @vitejs/plugin-vue2-jsx
+
+- [ ] `vite` 与 `TS` 又有什么联系和限制？
+- [ ] `esbuild`
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import './assets/styles/reset.less'
+import './assets/styles/custom.less'
 import { plus } from './utils/utils.js'
 // 在浏览器中利用原生ES导入 该语法会报错。因此vite需要预构建 Uncaught TypeError: Failed to resolve module specifier "lodash". Relative references must start with either "/", "./", or "../".
 import { camelCase } from 'lodash'
@@ -10,4 +12,11 @@ console.log(plus(1, 2))
 预构建的功能，体现在两方面：
 1. 将CommonJS或者UMD转换为 ESM 规范，以便浏览器能够支持原生导入
 2. 对第三方node_modules模块内的依赖作处理，转换模块路径，譬如：/node_modules/.vite/deps/lodash.js?v=d707e584
+
+css处理，支持less sass stylus等等
+需要安装对应的处理器，譬如 yarn add less -D
+
+为什么webpack处理less时需要安装less以及less-loader 而vite只需要安装less呢？
+其实处理less文件，需要的核心就是less库，它提供了API将less转化为css。在vite中内置了这些判断和转化。
+而webpack属于模块打包器，没有内置less转换css的功能，所以需要less-loader中的代码执行less转换css的逻辑
 */
