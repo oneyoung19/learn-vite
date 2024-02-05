a445633eec668e704053e4e51768072fd3ee8734
yangxiaoping <2878847069@qq.com>
2024-02-05 20:39:00 +0800

    feat: 测试babel用法

---
+++ src/vue/babel-utils-demo.mjs
@@ -0,0 +1,45 @@
+/*
+@babel/core已包括以下四种库：
+
+@babel/parser
+@babel/types
+@babel/traverse
+@babel/generator
+*/
+import babel from '@babel/core'
+import generator from '@babel/generator'
+const babelGenerator = generator.default
+// import babelParser from '@babel/parser'
+
+const template = `<p>Hello World</p>` // html cant be parsed
+
+const script = `console.log('Hello World')`
+
+const ast = babel.parse(script)
+// console.log(ast)
+// console.log(babelParser.parseExpression(script))
+
+const visitor = {
+  CallExpression: {
+    enter (path) {
+      console.log('Enter CallExpression')
+      // path.addComment('trailing', ';', true)
+    },
+    exit () {
+      console.log('Exit CallExpression')
+    }
+  },
+  StringLiteral: {
+    enter (path) {
+      console.log('Enter StringLiteral')
+    },
+    exit () {
+      console.log('Exit StringLiteral')
+    }
+  }
+}
+
+const newAst = babel.traverse(ast, visitor)
+// console.log(babelGenerator)
+// console.log(babelGenerator(newAst))
+console.log(babel.transformFromAst(ast).code)
