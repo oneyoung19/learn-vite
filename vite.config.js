e6297a43d6d4bb1e6d2a27ea97646ed51eba04d0
yangxiaoping <2878847069@qq.com>
2024-06-10 18:41:54 +0800

    feat: 添加mode和base配置

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,28 @@
 import path from 'node:path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import legacyPlugin from '@vitejs/plugin-legacy'
 // import imagemin from 'unplugin-imagemin/vite'
 
-export default defineConfig({
-  plugins: [vue(), legacyPlugin()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
+export default defineConfig(({ mode }) => {
+  // env文件中的环境变量默认只会注入到客户端代码中 如果node环境下要使用 需要手动加载 https://vitejs.dev/guide/api-javascript.html#loadenv
+  const env = loadEnv(mode, process.cwd(), '')
+  // console.log(mode, env)
+  return {
+    base: env.NODE_ENV === 'production' ? '/learn-vite/' : '',
+    plugins: [vue(), legacyPlugin()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src')
+      }
+    },
+    build: {
+      // outDir: 'dist-manifest',
+      // manifest: true,
+      // rollupOptions: {
+      //   input: path.resolve(__dirname, 'src/main.js')
+      // }
     }
-  },
-  build: {
-    // outDir: 'dist-manifest',
-    // manifest: true,
-    // rollupOptions: {
-    //   input: path.resolve(__dirname, 'src/main.js')
-    // }
   }
 })
