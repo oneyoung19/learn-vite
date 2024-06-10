14bc9d034a8d5d0851d15bb7ec1df1db5000ea9b
yangxiaoping <2878847069@qq.com>
2024-06-10 17:19:19 +0800

    fix: imagemin不兼容github服务器

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,10 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import legacyPlugin from '@vitejs/plugin-legacy'
-import imagemin from 'unplugin-imagemin/vite'
+// import imagemin from 'unplugin-imagemin/vite'
 
 export default defineConfig({
-  plugins: [vue(), legacyPlugin(), imagemin({
-    // sharp or squoosh
-    mode: 'sharp'
-  })],
+  plugins: [vue(), legacyPlugin()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
